d8fb5ad88de04718795c9d863037f3176f9d85bc
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-15 20:41:54 +0200

    Update text_wrap.py

--- text_wrap.py
+++ text_wrap.py
@@ -1,3 +1,7 @@
+# Exercise: Text Wrap
+# URL: https://www.hackerrank.com/challenges/text-wrap/problem?isFullScreen=true
+# Description: Print substrings of a given size from a string 
+
 def wrap(string, max_width):
     wrapped_string = ""
     for i in range(0, len(string), max_width):
