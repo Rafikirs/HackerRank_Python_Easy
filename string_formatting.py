5a464397149c4e531705df377bc0cbaa48ba7dc8
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-15 20:47:57 +0200

    Update string_formatting.py

--- string_formatting.py
+++ string_formatting.py
@@ -1,6 +1,9 @@
+# Exercise: String Formatting
+# URL: https://www.hackerrank.com/challenges/python-string-formatting/problem?isFullScreen=true
+# Description: Given an integer n, print the decimal, octal, hexadecimal and binary values for each integer from 1 to n
+
 def print_formatted(number):
-    width = len(bin(number)) - 2  # Width based on the binary length of the number
+    width = len(bin(number)) - 2  
 
     for i in range(1, number + 1):
-        # Using string formatting to ensure proper alignment and width
         print(f"{i:>{width}} {oct(i)[2:]:>{width}} {hex(i)[2:].upper():>{width}} {bin(i)[2:]:>{width}}")
