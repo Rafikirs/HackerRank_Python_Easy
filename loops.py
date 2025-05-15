4fdf698da2a79bc3c0304e257c29e0d4bcf4cc06
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-15 20:11:24 +0200

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,3 +1,7 @@
+# Exercise: Loops
+# URL: https://www.hackerrank.com/challenges/python-loops?isFullScreen=true
+# Description: Create a simple loop
+
 if __name__ == '__main__':
     n = int(input())
     for i in range(n):
