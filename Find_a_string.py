e4f3bf300d0f822270a6ea760b19b699d77d0d91
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-15 20:32:32 +0200

    Update and rename count_substring.py to Find_a_string.py

--- Find_a_string.py
+++ Find_a_string.py
@@ -1,3 +1,7 @@
+# Exercise: Find a string
+# URL: https://www.hackerrank.com/challenges/find-a-string/problem?isFullScreen=true
+# Description: Count how many times a given substring appears in a string
+
 def count_substring(string, sub_string):
     count = 0
     for i in range(len(string) - len(sub_string) + 1):
