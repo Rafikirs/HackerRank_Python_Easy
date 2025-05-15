fa3e454733f6633be5d8bd93563477eeed897a65
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-15 20:07:56 +0200

    Update arithmetic_operators.py

--- arithmetic_operators.py
+++ arithmetic_operators.py
@@ -1,7 +1,11 @@
+# Exercise: Arithmetic Operators
+# URL: https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true 
+# Description: Print sum, difference and division
+
 if __name__ == '__main__':
     a = int(input())
     b = int(input())
 
-    print(a + b)     # Somme
-    print(a - b)     # Différence (a - b)
-    print(a * b)     # Produit
+    print(a + b)     
+    print(a - b)     
+    print(a * b)     
