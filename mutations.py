55e5deed66273106f3afd371aaa8548258e6a6ab
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-15 20:29:54 +0200

    Update mutations.py

--- mutations.py
+++ mutations.py
@@ -1,2 +1,6 @@
+# Exercise: Mutations
+# URL: https://www.hackerrank.com/challenges/python-mutations/problem?isFullScreen=true
+# Description: Change one letter from a string at a given position
+
 def mutate_string(string, position, character):
     return string[:position] + character + string[position+1:]
