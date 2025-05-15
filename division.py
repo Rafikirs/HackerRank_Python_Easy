1f5ed4e29fd371fe003807d0407fe53390847a89
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-15 20:09:35 +0200

    Update and rename division.py to python_division.py

--- division.py
+++
@@ -1,6 +0,0 @@
-if __name__ == '__main__':
-    a = int(input())
-    b = int(input())
-
-    print(a // b)  # Division entière
-    print(a / b)   # Division flottante
---
+++ python_division.py
@@ -0,0 +1,10 @@
+# Exercise: Python Division
+# URL: https://www.hackerrank.com/challenges/python-division/problem?isFullScreen=true
+# Description: Print rounded division and real division
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    print(a // b)  
+    print(a / b)  
