d75d8850fd00ca209ed0f3b64aeea3abfe75d431
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-15 20:27:03 +0200

    Update what's_your_name.py

--- what's_your_name.py
+++ what's_your_name.py
@@ -1,2 +1,6 @@
+# Exercise: What's your name
+# URL: https://www.hackerrank.com/challenges/whats-your-name/problem?isFullScreen=true
+# Description: Put variables into a string
+
 def print_full_name(first, last):
     print(f"Hello {first} {last}! You just delved into python.")
