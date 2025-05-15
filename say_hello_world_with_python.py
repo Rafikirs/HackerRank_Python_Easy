c96b657df9445318447f2a3e835f882f54296a8f
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-15 20:04:28 +0200

    Update and rename hello_world.py to say_hello_world_with_python.py

---
+++ say_hello_world_with_python.py
@@ -0,0 +1,6 @@
+# Exercise: Say "Hello World!" With Python
+# URL: https://www.hackerrank.com/challenges/py-hello-world/problem?isFullScreen=true
+# Description: Print "Hello World!"
+
+if __name__ == '__main__':
+    print("Hello World")
