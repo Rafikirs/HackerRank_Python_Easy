67a8c9ff9ef19d1ba58151059d0c3d9d5e584e56
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-16 11:09:36 +0200

    Update check_strict_superset.py

--- check_strict_superset.py
+++ check_strict_superset.py
@@ -1,3 +1,7 @@
+# Exercise: Check Strict Superset
+# URL: https://www.hackerrank.com/challenges/py-check-strict-superset/problem?isFullScreen=true
+# Description: Check whether a set is a strict superset of another set 
+
 A = set(input().split())
 n = int(input())
 result = all(A > set(input().split()) for _ in range(n))
