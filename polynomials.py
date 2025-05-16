b3053253ae9cb1823b32f712e6e62ea06e341841
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-16 11:30:26 +0200

    Update polynomials.py

--- polynomials.py
+++ polynomials.py
@@ -1,3 +1,7 @@
+# Exercise: Polynomials
+# URL: https://www.hackerrank.com/challenges/np-polynomials/problem?isFullScreen=true
+# Description: Find the value of a polynomial at a certain point
+
 import numpy as np
 
 coefficients = list(map(float, input().split()))
