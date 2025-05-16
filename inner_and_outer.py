4a4032462d8d60624b72fdd6d5946b653f72746e
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-05-16 11:28:02 +0200

    Update inner_and_outer.py

--- inner_and_outer.py
+++ inner_and_outer.py
@@ -1,3 +1,7 @@
+# Exercise: Inner and Outer 
+# URL: https://www.hackerrank.com/challenges/np-inner-and-outer/problem?isFullScreen=true
+# Description: Print the inner and outer products of two arrays
+
 import numpy as np
 
 A = np.array(list(map(int, input().split())))
