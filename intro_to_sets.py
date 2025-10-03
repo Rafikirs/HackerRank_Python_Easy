b917f334c2fe1b5106092466f49b94360bef01bc
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-10-03 09:31:28 +0200

    Create intro_to_sets.py

---
+++ intro_to_sets.py
@@ -0,0 +1,6 @@
+# Exercise: Introduction to Sets
+# URL: https://www.hackerrank.com/challenges/py-introduction-to-sets/problem?isFullScreen=true
+# Description: Using a set
+
+def average(array):
+    return sum(set(array)) / len(set(array))
