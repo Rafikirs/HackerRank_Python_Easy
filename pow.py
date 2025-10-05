4bd453c1b4c3546afe158e6d04c47537aa412233
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-10-05 21:19:50 +0200

    Create pow.py

---
+++ pow.py
@@ -0,0 +1,10 @@
+# Exercise: Power - Mod Power
+# URL: https://www.hackerrank.com/challenges/python-power-mod-power/problem?isFullScreen=true
+# Description: Using pow built-in Python function
+
+a = int(input())
+b = int(input())
+m = int(input())
+
+print(pow(a, b))
+print(pow(a, b, m))
