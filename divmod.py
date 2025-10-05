a92c0dc7477f1bc7af0f49d83702325b020e3c20
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-10-05 21:16:25 +0200

    Create divmod.py

---
+++ divmod.py
@@ -0,0 +1,10 @@
+# Exercise: Mod Divmod
+# URL: https://www.hackerrank.com/challenges/python-mod-divmod/problem?isFullScreen=true
+# Description: Using divmod built-in Python function
+
+a = int(input())
+b = int(input())
+
+print(divmod(a, b)[0])
+print(divmod(a, b)[1])
+print(divmod(a, b))
