4414b2c309299203b5a0b2462c2b399fcede494a
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-10-06 11:55:40 +0200

    Create integers_all_sizes.py

---
+++ integers_all_sizes.py
@@ -0,0 +1,10 @@
+# Exercise: Integers Come In All Sizes
+# URL: https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem?isFullScreen=true
+# Description: Working on very large integers
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+print(a**b + c**d)
