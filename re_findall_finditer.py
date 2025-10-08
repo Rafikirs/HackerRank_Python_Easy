e0f80ff62cb7725f2b15560e8cf8a92ab0958834
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-10-08 20:34:52 +0200

    Create re_findall_finditer.py

---
+++ re_findall_finditer.py
@@ -0,0 +1,3 @@
+# Exercise: Re.findall() and Re.finditer()
+# URL: https://www.hackerrank.com/challenges/re-findall-re-finditer/problem?isFullScreen=true
+# Description: Using re.findall() and re.finditer() from regex
