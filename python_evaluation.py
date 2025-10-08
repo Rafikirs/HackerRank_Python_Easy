1c0089bf260ff117ed1a094c385e4753ac038d5c
Raphaël Sisso <sisso.raphael1@gmail.com>
2025-10-08 20:31:30 +0200

    Create python_evaluation.py

---
+++ python_evaluation.py
@@ -0,0 +1,6 @@
+# Exercise: Python Evaluation
+# URL: https://www.hackerrank.com/challenges/python-eval/problem?isFullScreen=true
+# Description: Evaluating an expression
+
+var = input()
+eval(var)
